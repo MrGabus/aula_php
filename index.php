644783c2515df6dcb19058696bd465684dc6236c
MrGabus <gabrielsiodoni@hotmail.com>
2020-11-08 13:03:21 -0300

    Criação de serviços e funções PHP

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-session_start()
+
 ?>
 
 <!DOCTYPE html>
@@ -17,17 +17,19 @@ session_start()
 
         <form action="script.php" method="post">
             <?php
-            $mensagemDeSucesso = isset($_SESSION['mensagem-de-sucesso']) ? $_SESSION['mensagem-de-sucesso'] :'';
-            if(!empty($mensagemDeSucesso))
-            {
-                echo $mensagemDeSucesso;
-            }
+                include "servicos/servicoMensagemSessao.php";
 
-            $mensagemDeErro = isset($_SESSION['mensagem-de-erro']) ? $_SESSION['mensagem-de-erro'] :'';
-            if(!empty($mensagemDeErro))
-            {
-                echo $mensagemDeErro;
-            }
+                $mensagemDeSucesso = obterMensagemSucesso();
+                if(!empty($mensagemDeSucesso))
+                {
+                    echo $mensagemDeSucesso;
+                }
+
+                $mensagemDeErro = obterMensagemErro();
+                if(!empty($mensagemDeErro))
+                {
+                    echo $mensagemDeErro;
+                }
             ?>
             <p>Seu nome: <input type="text" name="nome"/></p>
             <p>Sua idade: <input type="text" name="idade"/></p>
---
+++ servicos/servicoCategoriaCompetidor.php
@@ -0,0 +1,57 @@
+<?php
+
+
+function defineCategoriaCompetidor(string $nome, string $idade) : ?string
+{
+    $categorias = [];
+    $categorias[] = 'intantil';
+    $categorias[] = 'adolecente';
+    $categorias[] = 'adulto';
+
+    if(validaNome($nome) && validaIdade($idade))
+    {
+        removerMensagemErro();
+        
+        if($idade >= 6 && $idade <=12)
+        {
+            for($i = 0; $i <= count($categorias); $i++)
+            {
+                if($categorias[$i] == 'intantil')
+                {
+                    setarMensagemSucesso($mensagem= "O nadador " .$nome. " compete na categoria " .$categorias[$i]);                
+                    return null;
+                }        
+            }
+        }
+
+        else if($idade >= 13 && $idade <=18)
+        {
+            for($i = 0; $i <= count($categorias); $i++)
+            {
+                if($categorias[$i] == 'adolecente')
+                {
+                    setarMensagemSucesso($mensagem= 'O nadador ' .$nome. " compete na categoria " .$categorias[$i]);                
+                    return null;
+                }        
+            }
+        }
+
+        else
+        {
+            for($i = 0; $i <= count($categorias); $i++)
+            {
+                if($categorias[$i] == 'adulto')
+                {
+                    setarMensagemSucesso($mensagem= "O nadador " .$nome. " compete na categoria " .$categorias[$i]);                
+                    return null;
+                }        
+            }
+        }
+    }
+
+    else 
+    {
+        removerMensagemSucesso();
+        return obterMensagemErro();
+    }
+}
